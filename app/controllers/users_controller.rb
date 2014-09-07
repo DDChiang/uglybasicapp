37b31b82a5c5dae4666f3a7fcfa9c6de2f344db2
Deedee Chiang <DDChiang@users.noreply.github.com>
2014-09-07 10:24:41 -0700

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
       
     if @user.save 
+	  sign_in @user
 	  flash[:success] = "Welcome to the Sample Apple!"
 	  redirect_to @user
 	else
